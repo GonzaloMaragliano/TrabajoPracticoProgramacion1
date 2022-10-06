7c4e4efe6eeed8518b7edc9e591b735e080cc48c
nehuengiacone <113162080+nehuengiacone@users.noreply.github.com>
2022-10-06 17:55:30 -0300

    Primer Commit
    + Creacion de los archivos RepositorioFactura y Factura.
    + class Factura se le agrego los metodos 'get' de sus atributos.
    + class RepositorioFactura se armo el molde de la clase.

---
+++ RepositorioFactura.php
@@ -0,0 +1,25 @@
+<?php
+
+    // Objetos requeridos para llamada al repositorio.
+    require_once 'Factura.php';
+    require_once 'Usuario.php';
+
+    class RepositorioFactura {
+        
+        // LLamada a la base de datos para dar de alta una factura por un usuario.
+        public function altaFactura($factura, $usuario){
+
+            $q = "INSERTAR DATOS DE LA FACTURA A LA BASE DE DATOS";
+            
+        }
+
+
+
+
+
+    }
+
+
+
+
+?>
