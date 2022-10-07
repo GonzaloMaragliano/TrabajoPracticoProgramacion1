f97ae1307587e3361c950da6b74b7f5f4670c633
nehuengiacone <113162080+nehuengiacone@users.noreply.github.com>
2022-10-07 02:40:47 -0300

    RepositorioFactura
    Cambios en la clase RepositorioFactura
    + Se agrego los metodos bajaFactura() y verFactura() (no funcionales)

--- RepositorioFactura.php
+++ RepositorioFactura.php
@@ -1,19 +1,35 @@
 <?php
 
     // Objetos requeridos para llamada al repositorio.
+
     require_once 'Factura.php';
     require_once 'Usuario.php';
 
     class RepositorioFactura {
         
         // LLamada a la base de datos para dar de alta una factura por un usuario.
+        
         public function altaFactura($factura, $usuario){
 
             $q = "INSERTAR DATOS DE LA FACTURA A LA BASE DE DATOS";
             
         }
 
+        // LLamada a la base de datos para dar de baja una factura por un usuario.
 
+        public function bajaFactura($factura, $usuario){
+
+            $q = "ACTUALIZAR DATOS DE LA FACTURA A LA BASE DE DATOS";
+
+        }
+
+        // LLamada a la base de datos para ver las facturas.
+
+        public function verFacturas(){
+
+            $q = "SELECCIONAR TODAS LAS FACTURAS DE LA BASE DE DATOS";
+
+        }
 
 
 
