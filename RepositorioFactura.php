3820c5d0e9685a48475e4b074832a5185fd247e3
nehuengiacone <113162080+nehuengiacone@users.noreply.github.com>
2022-10-09 16:05:35 -0300

    AltaFactura
    Se escribio el metodo para dar alta de factura

--- RepositorioFactura.php
+++ RepositorioFactura.php
@@ -16,10 +16,14 @@
         public function __construct(){
 
             if(is_null(self::$conexion)){
+                $credenciales = credenciales();
+
                 try{
-                    $repo = new mysqli(credenciales(), credenciales(), credenciales(), credenciales());
+                    self::$conexion = new mysqli($credenciales['servidor'], $credenciales['usuario'], $credenciales['clave'], $credenciales['BD']);
+
                 }catch(Exception $e){
                     echo $e;
+                    die(self::$conexion);
                 }  
             }
 
@@ -35,17 +39,70 @@
 
 
 
-
         // LLamada a la base de datos para dar de alta una factura por un usuario.
 
         public function altaFactura($factura, $usuario){
 
-            $q = "INSERTAR DATOS DE LA FACTURA A LA BASE DE DATOS";
+            $nro_factura = $factura->getNroFactura();
+            $cliente = $factura->getCliente();
+            $f_alta = $factura->getFecha_Alta();
+            $f_facturacion = $factura->getFechaFacturacion();
+            $bultos = $factura->getBultos();
+            $obs = $factura->getObservacion();
+
+            $q = "INSERT INTO facturas (nro_factura, cliente, fecha_facturacion, fecha_carga, bultos, observacion, usuario_alta) VALUES(?, ?, ?, ?, ?, ?, ?)";
+            
+            if(!($setencia = self::$conexion->prepare($q))){
+                echo "Fallo la preparacion de la consulta:  ".self::$conexion->errno."  ERROR: ".self::$conexion->error;
+            }
+
+            if(!$sentencia->bind_param("sssssis", $nro_factura, $cliente, $f_alta, $f_facturacion, $bultos, $obs)){
+                echo "Fallo la vinculacion de parametros de la consulta:  ".self::$conexion->errno."  ERROR: ".self::$conexion->error;
+            }
+            
+            if(!$sentencia->execute()){
+                echo "Fallo la ejecucion de la consulta:  ".self::$conexion->errno."  ERROR: ".self::$conexion->error;
+            }
             
 
-            
-            echo "Conexion exitosa";
+            $sentencia->close();
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         // LLamada a la base de datos para dar de baja una factura por un usuario.
 
